e6c16d11ecdc7aaf77800cbc70f78befccb87af4
Hordur Freyr Yngvason <hordurf@gmail.com>
2023-12-03 15:01:47 -0500

    Improve readability
    That nested loop was an eyesore

--- day-3.lisp
+++ day-3.lisp
@@ -48,10 +48,12 @@
                   ;; then increment
                   (setf digit-begin (or digit-begin col)
                         number (+ (* 10 number) (- (char-code char) (char-code #\0))))
+                  ;; else collect and reset
                   (when digit-begin
                     (collect (make-number-interval number row digit-begin col))
                     (setf digit-begin nil
                           number 0)))))
+          ;; end-of-row needs special handling
           (when digit-begin
             (collect (make-number-interval number row digit-begin cols))))))))
 
@@ -61,13 +63,12 @@
           (icols-begin (number-interval-col-begin interval))
           (icols-end (number-interval-col-end interval)))
       (loop :for row :from (1- irow) :upto (1+ irow) :do
-        (when (and (<= 0 row (1- rows)) ;; in the grid
-                   (loop :for col :from (1- icols-begin) :upto icols-end :do
-                     (when (and (<= 0 col (1- cols)) ;; in the grid
-                                (not (and (= row irow) (<= icols-begin col (1- icols-end)))) ;; not in the current interval
-                                (char/= #\. (aref grid row col))) ;; is a symbol
-                       (return t))))
-          (return t))))))
+        (when (<= 0 row (1- rows)) ;; in the grid
+          (loop :for col :from (1- icols-begin) :upto icols-end :do
+            (when (and (<= 0 col (1- cols)) ;; in the grid
+                       (not (and (= row irow) (<= icols-begin col (1- icols-end)))) ;; not in the current interval
+                       (char/= #\. (aref grid row col))) ;; is a symbol
+              (return-from symbol-adjacent-p t))))))))
 
 (defun load-input ()
   (read-file-string "day-3.input"))
