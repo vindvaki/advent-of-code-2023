3b3983ffc7795597e9ab07a7f7b1774b1d44dd19
Hordur Freyr Yngvason <hordurf@gmail.com>
2023-12-09 08:53:58 -0500

    Solve day 9

---
+++ day-9.lisp
@@ -0,0 +1,57 @@
+(defpackage #:advent-of-code-2023/day-9
+  (:use #:cl)
+  (:import-from #:serapeum
+                #:~>
+                #:lines)
+  (:import-from #:uiop
+                #:read-file-string)
+  (:import-from #:cl-ppcre
+                #:split)
+  (:import-from #:alexandria
+                #:last-elt))
+
+(in-package #:advent-of-code-2023/day-9)
+
+(defparameter *example* "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45")
+
+(defun load-input ()
+  (read-file-string "day-9.input"))
+
+(defun parse-input (input)
+  (~> (lines input)
+      (mapcar #'parse-line _)))
+
+(defun parse-line (line)
+  (~> (split " " line)
+      (mapcar #'parse-integer _)))
+
+(defun sequential-differences (numbers)
+  (loop for (a b) on numbers while b collecting (- b a)))
+
+(defun difference-tower (numbers)
+  (loop for sequence = numbers then differences
+        for differences = (sequential-differences sequence)
+        collect sequence
+        while (cdr (remove-duplicates sequence))))
+
+(defun extrapolate-history (numbers)
+  (let ((tower (difference-tower numbers)))
+    (loop for differences in (reverse tower)
+          for result = (last-elt differences) then (+ result (last-elt differences))
+          finally (return result))))
+
+(defun part-1 (input)
+  (loop for numbers in (parse-input input)
+        summing (extrapolate-history numbers)))
+
+(defun reverse-extrapolate-history (numbers)
+  (let ((tower (difference-tower numbers)))
+    (loop for differences in (reverse tower)
+          for result = (car differences) then (- (car differences) result)
+          finally (return result))))
+
+(defun part-2 (input)
+  (loop for numbers in (parse-input input)
+        summing (reverse-extrapolate-history numbers)))
